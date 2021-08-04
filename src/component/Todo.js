a178c73b17b0d88cecf6167ebc2e468568662a88
Prashant Sawant <prashant.sawant@neosoftmail.com>
2021-08-04 15:33:05 +0530

    esling config

---
+++ src/component/Todo.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const Todo = ({}) => {
+  const todoList = [
+    {
+      id: 1,
+      dish: dosha,
+    },
+    {
+      id: 2,
+      dish: kabab,
+    },
+  ];
+
+  return (
+    <div data-testid="todo-1">
+      <h1>Todo</h1>
+    </div>
+  );
+};
